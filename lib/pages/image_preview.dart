de2b89d5ce81a72ebd6bccf21230ee5b3e85de8d
Akash Harikumar <akashharikumar2013@gmail.com>
2023-05-15 21:57:57 +0530

    Implemented static view with slight offset in detection

--- lib/pages/image_preview.dart
+++ lib/pages/image_preview.dart
@@ -1,25 +1,175 @@
 import 'dart:io';
 
 import 'package:camera/camera.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:image/image.dart' as img;
 
-class ImagePreview extends StatelessWidget {
+class ImagePreview extends StatefulWidget {
   final XFile file;
 
-  const ImagePreview(this.file, {required key});
+  const ImagePreview(this.file, {Key? key}) : super(key: key);
+
+  @override
+  ImagePreviewState createState() => ImagePreviewState();
+}
+
+class ImagePreviewState extends State<ImagePreview> {
+  Offset? _selectedPixel;
+  String? selectedHexColor;
+  Offset? cursorPosition;
+  Color? _selectedColor;
+
+  void _handleDragStart(DragStartDetails details) {
+    setState(() {
+      cursorPosition = details.localPosition;
+    });
+  }
+  void _handleDragEnd(DragEndDetails details) {
+    setState(() {
+      _selectedColor = _getColorAtPixel(cursorPosition);
+      if (kDebugMode) {
+        print("Color: End ");
+        print(_selectedColor);
+        print("Cursor Position: ");
+        print(cursorPosition);
+      }
+    });
+  }
+  void _handleDragUpdate(DragUpdateDetails details) {
+    setState(() {
+      // RenderBox box = context.findRenderObject() as RenderBox;
+      // cursorPosition = box.globalToLocal(details.globalPosition);
+      cursorPosition = details.localPosition;
+      if (kDebugMode) {
+        print("Color: Update ");
+        print(selectedHexColor);
+        print("Cursor Position Update: ");
+        print(cursorPosition);
+      }
+    });
+  }
+
+  Color? _getColorAtPixel(Offset? pixel) {
+    if (pixel == null) return null;
+
+    final img.Image image = img.decodeImage(File(widget.file.path).readAsBytesSync())!;
+    final int x = pixel.dx.toInt();
+    final int y = pixel.dy.toInt();
+
+    if (x >= 0 && x < image.width && y >= 0 && y < image.height) {
+      final img.Pixel pixelValue = image.getPixel(x, y);
+      final int alpha = (pixelValue.a as int).clamp(0, 255);
+      final int red = (pixelValue.r as int).clamp(0, 255);
+      final int green = (pixelValue.g as int).clamp(0, 255);
+      final int blue = (pixelValue.b as int).clamp(0, 255);
+      final int hexValue = ((alpha << 24) | (red << 16) | (green << 8) | blue) & 0xFFFFFFFF;
+      // return '#${hexValue.toRadixString(16).padLeft(8, '0')}';
+      return Color.fromARGB(alpha, red, green, blue);
+
+    }
+
+    return null;
+  }
+
+
+  String _getHexCode(Color? color) {
+    if (color == null) return '';
+    return '#${color.value.toRadixString(16).padLeft(8, '0').substring(2)}';
+  }
+
+  String _getShade(Color? color) {
+    if (color == null) return '';
+    final double luminance = color.computeLuminance();
+    if (luminance > 0.5) {
+      return 'Light';
+    } else {
+      return 'Dark';
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Image Preview'),
+        title: const Text('Static View'),
       ),
-      body: Center(
-        child: Image.file(
-          File(file.path),
-          fit: BoxFit.contain,
-        ),
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          final imageSize = Size(
+            constraints.maxWidth,
+            constraints.maxHeight,
+          );
+          return GestureDetector(
+            onPanStart: _handleDragStart,
+            onPanUpdate: _handleDragUpdate,
+            onPanEnd: _handleDragEnd,
+            child: Stack(
+              alignment: Alignment.center,
+              children: [
+                Center(
+                  child: Image.file(
+                    File(widget.file.path),
+                    fit: BoxFit.contain,
+                  ),
+                ),
+                if (cursorPosition != null)
+                Positioned(
+                left: cursorPosition!.dx, // Adjust the position as needed
+                top: cursorPosition!.dy, // Adjust the position as needed
+                child: Container(
+                width: 20,
+                height: 20,
+                decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                border: Border.all(
+                color: Colors.red,
+                width: 3.0,
+                ),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  left: 0,
+                  right: 0,
+                  bottom: 0,
+                  child: Container(
+                    height: 80,
+                    color: Colors.black,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          width: 40,
+                          height: 40,
+                          color: _selectedColor,
+                        ),
+                        const SizedBox(width: 8),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Hex Code: ${_getHexCode(_selectedColor)}',
+                              style: const TextStyle(color: Colors.white),
+                            ),
+                            Text(
+                              'Shade: ${_getShade(_selectedColor)}',
+                              style: const TextStyle(color: Colors.white),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          );
+        },
       ),
     );
   }
+
 }
+
--- lib/pages/pixel.dart
+++ lib/pages/pixel.dart
@@ -1,1 +1,101 @@
+import 'dart:io';
+import 'dart:ui';
+
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:image/image.dart' as img;
+
+class ImagePreview extends StatefulWidget {
+  final XFile file;
+
+  const ImagePreview(this.file, {required Key key}) : super(key: key);
+
+  @override
+  State<ImagePreview> createState() => _ImagePreviewState();
+}
+
+
+class _ImagePreviewState extends State<ImagePreview> {
+  Offset? selectedPixel;
+  Color? selectedColor;
+
+  void _handleTapUp(TapUpDetails details) {
+    setState(() {
+      final RenderBox box = context.findRenderObject() as RenderBox;
+      selectedPixel = box.globalToLocal(details.globalPosition);
+      selectedColor = _getColorAtPixel(selectedPixel);
+    });
+  }
+
+  Color? _getColorAtPixel(Offset? pixel) {
+    if (pixel == null) return null;
+
+    final img.Image image = img.decodeImage(File(widget.file.path).readAsBytesSync())!;
+    final int x = pixel.dx.toInt();
+    final int y = pixel.dy.toInt();
+
+    if (x >= 0 && x < image.width && y >= 0 && y < image.height) {
+      final img.Pixel pixelValue = image.getPixel(x, y);
+      final int alpha = (pixelValue.a as int).clamp(0, 255);
+      final int red = (pixelValue.r as int).clamp(0, 255);
+      final int green = (pixelValue.g as int).clamp(0, 255);
+      final int blue = (pixelValue.b as int).clamp(0, 255);
+      return Color.fromARGB(alpha, red, green, blue);
+    }
+
+    return null;
+  }
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Image Preview'),
+      ),
+      body: GestureDetector(
+        onTapUp: _handleTapUp,
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            Center(
+              child: Image.file(
+                File(widget.file.path),
+                fit: BoxFit.contain,
+              ),
+            ),
+            if (selectedPixel != null && selectedColor != null)
+              Positioned(
+                left: selectedPixel!.dx,
+                top: selectedPixel!.dy,
+                child: Container(
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    border: Border.all(
+                      color: Colors.white,
+                      width: 2.0,
+                    ),
+                  ),
+                  child: SizedBox(
+                    width: 50.0,
+                    height: 50.0,
+                    child: Center(
+                      child: Text(
+                        selectedColor!.toString(),
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+}
 
